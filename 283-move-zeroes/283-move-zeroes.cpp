2787798eeec7304fe7130006c9018587fefd513a
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-06-23 17:11:57 +0530

    Time: 41 ms (35.28%), Space: 19.3 MB (57.32%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+       for(int i=0,j=0;i<n;i++){
+           if(nums[i]!=0){
+               swap(nums[j++],nums[i]);
+           }
+       }
+    }
+};
