806fdb2029729db1bd8987275f4f8e85e850e949
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-06-24 16:54:31 +0530

    Time: 163 ms (5.02%), Space: 32.7 MB (5.05%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+      int n=nums.size();
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            nums[i]=nums[i]+nums[i-1];
+            ans.push_back(nums[i]);
+        }
+        int m=ans.size();       
+        int prev;
+        int next;
+        int temp=0;
+        int x;
+        for(auto i:ans){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<m;i++){
+            if(i==0){
+                prev=0;
+            }
+            else{
+            prev=ans[i-1];  
+            }
+            next=ans[m-1]-ans[i];
+            if(prev==next){
+                temp=1;
+                x=i;
+                
+                break;
+            }
+        }
+        if(temp==0){
+            return -1;
+        }
+        else{
+            return x;
+        }
+    }
+};
