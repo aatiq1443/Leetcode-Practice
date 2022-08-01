c946fe3f1ca52f9626cbab217789dda058f07fe5
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-08-01 07:17:13 +0530

    Time: 22 ms (18.78%), Space: 14.3 MB (17.22%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
-private:
+public:
     void solve(vector<int> arr ,  vector<vector<int>> &ans , vector<int> ds , int n , int target , int ind){
-        
-            if(target==0){
-                ans.push_back(ds);
-                return ;
-            }
-                
-         
+        if(target==0){
+            ans.push_back(ds);
+            return ;
+        }
         for(int i=ind;i<n;i++){
             if(i>ind && arr[i]==arr[i-1]) continue;
             if(arr[i]>target) break;
             ds.push_back(arr[i]);
-            solve(arr, ans, ds, n , target - arr[i] , i+1);
+            solve(arr,ans,ds,n,target-arr[i],i+1);
             ds.pop_back();
         }
+        
     }
-public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        int n=candidates.size();
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
         vector<vector<int>> ans;
-        vector<int>ds;
-        solve(candidates , ans , ds , n, target , 0);
+        vector<int> ds;
+        solve(nums,ans,ds,n,target,0);
         return ans;
     }
 };
