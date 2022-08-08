3c01be3e019e49049a9cc3e3a8328c00b500006b
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-08-08 16:02:57 +0530

    Time: 184 ms (75.10%), Space: 18.4 MB (42.79%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        
-        int temp=accumulate(nums.begin(),nums.end(),0);
-        int n=nums.size();
-        int data=temp/2;
-        cout<<data<<" "<<temp;
-        if(temp%2==0){
-            
-            int dp[n+1][data+1];
-            for(int i=0;i<n+1;i++){
-                for(int j=0;j<data+1;j++){
-                    if(i==0){
-                        dp[i][j]=false;
-                    }
-                    if(j==0){
-                        dp[i][j]=true;
-                    }
-                }
-            }
-            for(int i=1;i<n+1;i++){
-                for(int j=1;j<data+1;j++){
-                    if(nums[i-1]<=j){
-                        dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    }
-                    else if(nums[i-1]>j){
-                        dp[i][j]=dp[i-1][j];
-                    }
-                }
-            }
-               
-            return dp[n][data];     
-        }
-        else{
-            return false;
-        }
+    bool canPartition(vector<int>& ans) {
+        int n=ans.size();
+       	int temp=accumulate(ans.begin(),ans.end (),0);
+	int sum=temp/2;
+	int dp[n+1][sum+1];
+	if(temp%2==0){
+		for(int i=0;i<n+1;i++){
+			for(int j=0;j<sum+1;j   ++){
+				if(i==0) dp[i][j]=false;
+				if(j==0) dp[i][j]=true;
+			}
+		}
+
+		for(int i=1;i<n+1;i++){
+			for(int j=1;j<sum+1;j++){
+				if(ans[i-1]<=j){
+					dp[i][j]=dp[i-1][j-ans[i-1]] || dp[i-1][j];
+				}
+				else if(ans[i-1]>j){
+					dp[i][j]=dp[i-1][j];
+				}
+			}
+		}
+		return dp[n][sum];
+	}
+	else{
+		return false;
+	}
     }
 };
