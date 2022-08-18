f3ba1790f9eb1f819d7430f4aaf55485d1d894e3
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-08-18 06:35:38 +0530

    Time: 33 ms (45.11%), Space: 19.5 MB (92.21%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>mp;
-        for(auto i:nums){
-            mp[i]++;
-        }
-        int maxi=0;
-        int val;
-        for(auto i:mp){
-            if(i.second>maxi){
-                maxi=i.second;
-                val=i.first;
+    int findCand(vector < int> &arr  , int n){
+        int maj_ele=0;
+        int count=1;
+        
+        for(int i=1;i<n;i++){
+            if(arr[maj_ele]==arr[i]){
+                count++;
+            }
+            else
+                count--;
+            if(count==0){
+                maj_ele=i;
+                count=1;
             }
         }
-        return val;
+        
+        return arr[maj_ele];
+    }
+    
+    bool isMajority(vector<int> & arr , int n  , int cand){
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==cand){
+                count++;
+            }
+        }
+        if(count>n/2){
+            return 1;
+        }
+        else
+            return 0;;
+    }
+    int majorityElement(vector<int>& nums) {
+        
+        int n=nums.size();
+        int cand =findCand(nums,n);
+        if(isMajority(nums,n,cand)){
+            return cand;
+        }
+        return -1;
     }
 };
