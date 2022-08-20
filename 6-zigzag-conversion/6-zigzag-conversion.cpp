f4329b7a62ff1aa8f21b4466ec567ea4d14ffee1
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-08-20 23:32:02 +0530

    Time: 17 ms (55.09%), Space: 8.2 MB (81.55%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int r) {
+        string ans = "";
+        int n=s.size();
+        int diff = (r-1)*2, up,down;
+        if(n<=r || r==1) return s;
+        for(int i=0; i<n; i=i+diff){
+            ans.push_back(s[i]);
+        }
+        for(int i=1; i<r-1; i++){
+            up = i*2;
+            down = (r-i-1)*2;
+            bool flag = true;
+            for(int j=i; j<n;){
+                ans.push_back(s[j]);
+                if(flag){
+                    j=j+down;
+                } else {
+                    j=j+up;
+                }
+                flag = !flag;
+            }
+        }
+        for(int i=r-1; i<n; i=i+diff){
+            ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
