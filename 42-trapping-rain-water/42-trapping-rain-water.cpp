50de0fe5b78b36b4493cd4968fc0928b0f1ff292
aatiq1443 <77941370+aatiq1443@users.noreply.github.com>
2022-08-25 15:59:44 +0530

    Time: 34 ms (22.13%), Space: 19.7 MB (82.05%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-   
-    int trap(vector<int>& nums) {
-        
-        
-        int n=nums.size();
-        if(n==1){
-            return 0;
-        }
+    int trap(vector<int>& height) {
+        int n=height.size();
+        if(n==1 ) return 0;
         int left=0;
         int right=n-1;
         
-        int maxLeft=0;
-        int maxRight=0;
+        int maxleft=0;
+        int maxright=0;
         
-        int result=0;
+        int res=0;
         
         while(left<=right){
             
-            if(nums[left]<=nums[right]){
-                if(nums[left]>=maxLeft){
-                    maxLeft=nums[left];
+            if(height[left]<=height[right]){
+                if(height[left] > maxleft){
+                    maxleft = height[left];
                 }
                 else{
-                    result= result + maxLeft - (nums[left]);
+                    res+= maxleft - height[left];
                 }
                 left++;
             }
-            
-            if(nums[right]<=nums[left]){
-                if(nums[right]>=maxRight){
-                    maxRight=nums[right];
+            if(height[left]>=height[right]){
+                if(height[right] > maxright){
+                    maxright = height[right];
                 }
                 else{
-                    result= result + maxRight - (nums[right]);
+                    res+= maxright - height[right];
                 }
                 right--;
             }
             
         }
-        return result;
+        return res;
+        
     }
 };
