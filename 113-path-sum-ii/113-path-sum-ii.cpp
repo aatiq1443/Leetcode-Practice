0392fa2f86e2c409c22b483a1a509c49c425625c
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-08-28 16:32:22 +0530

    Time: 22 ms (48.64%), Space: 19.9 MB (81.74%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -12,24 +12,20 @@
 class Solution {
 public:
     
-    void solve(TreeNode *root , int sum , vector<vector<int>> &ans , vector<int> ds){
+    void solve(TreeNode *root , int sum , vector<vector<int>> &ans , vector<int>&ds){
         if(!root) return ;
         
-        if(!root->left && !root->right) {
-            
-            if(sum==root->val){
-                ds.push_back(root->val);
+         ds.push_back(root->val);
+        if(sum==root->val && !root->left && !root->right) {
                 ans.push_back(ds);
-                return;
-            }
         }
         
-        ds.push_back(root->val);
+       
         
         solve(root->left , sum-root->val , ans , ds);
         solve(root->right , sum-root->val , ans , ds);
         
-       
+       ds.pop_back();
         
     }
     
