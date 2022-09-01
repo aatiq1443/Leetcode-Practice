061a7b8b9e48fe1de2e326bcd826c698573660ee
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-01 23:37:48 +0530

    Time: 268 ms (12.02%), Space: 86.4 MB (54.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -15,7 +15,10 @@ public:
      int ans=0;
     void pre(TreeNode* root,int max){
         if(!root)return;
-        if(root->val >= max){ ans++; max=root->val; }
+        if(root->val >= max){ 
+            ans++; 
+            max=root->val; 
+        }
         pre(root->left,max);
         pre(root->right,max);
     }
