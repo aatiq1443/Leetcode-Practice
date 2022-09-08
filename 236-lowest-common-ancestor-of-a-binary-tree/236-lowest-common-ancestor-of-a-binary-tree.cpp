fb70377eaf8a0ae70bdeb4fe8893e4fa2e2a32a1
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-08 12:18:26 +0530

    Time: 41 ms (9.01%), Space: 14.3 MB (57.09%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,23 +10,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-         if (!root)
-            return root;
         
-		// If p or q if found, return the root.
-        if (root == p || root == q)
-            return root;
+        if(!root) return NULL;
         
-		// Save the root found at left and right recursively
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(root == p || root == q) return root;
         
-		// If both the nodes are found, return parent of that node. That will be root.
-        if (left && right)
-            return root;
-        // If left is found, and right is not found.
-		// It is given that both the p and q will exist for sure.
-		// If left present then right will be a decendent of left, or vice versa.
-        return left? left: right;
+        TreeNode * left=lowestCommonAncestor(root->left , p , q);
+        TreeNode * right=lowestCommonAncestor(root->right , p ,q);
+        
+        if(left && right) return root;
+        
+        else if(left && !right) return left;
+        
+        else if(!left && right) return right;
+        
+        else return NULL;
+        
     }
 };
