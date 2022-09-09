81fd51aa7288a82be928e53aa8fa4c991f1728a9
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-09 23:04:21 +0530

    Time: 1577 ms (8.49%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -4,11 +4,11 @@ public:
 
      static bool compare(vector<int> &a, vector<int> &b)
      {
-          if (a[0] == b[0])
+          if (a[0]==b[0])
           {
-               return a[1] > b[1];
+               return a[1]>b[1];
           }
-          return a[0] < b[0];
+          return a[0]<b[0];
      }
     int numberOfWeakCharacters(vector<vector<int>>& nums) {
         
