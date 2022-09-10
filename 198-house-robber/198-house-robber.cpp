34af1ad78ac04b9d1f610d177622e98454bb84a9
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-10 15:38:54 +0530

    Time: 6 ms (17.23%), Space: 7.9 MB (25.88%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     
-    int solve(vector<int> &ans, vector<int> &dp , int n){
+//     int solve(vector<int> &ans, vector<int> &dp , int n){
         
-        if(n==0) ans[n];
+//         if(n==0) return ans[n];
         
-        if(n<0) return 0;
+//         if(n<0) return 0;
         
-        if(dp[n]!=-1) return dp[n];
+//         if(dp[n]!=-1) return dp[n];
         
-        int left= ans[n] + solve(ans , dp , n-2);
-        int right = 0+ solve(ans , dp , n-1);
+//         int left = ans[n] + solve(ans , dp , n-2);
         
-        return dp[n]=max(left , right);
+//         int right =0 + solve(ans , dp , n-1);
         
-    }
+//         return dp[n]=max(left , right);
+        
+//     }
     
+    int tabu(vector<int> &arr , vector<int> &dp , int n){
+        dp[0]=arr[0];
+        
+        for(int i=1;i<n;i++){
+            int left = arr[i] ;
+            if(i>1) left+=dp[i-2];
+            int right = 0 + dp[i-1];
+            
+            dp[i] = max(left, right);
+        }
+        return  dp[n-1];
+        
+    }        
+       
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n,0);
         
-        int ans=solve(nums , dp , n-1);
+        int ans=tabu(nums , dp , n);
         
         return ans;
         
