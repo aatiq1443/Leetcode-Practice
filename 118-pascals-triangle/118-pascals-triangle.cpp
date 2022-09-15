5483f0c7bda932b0412d5b08af8701ca64fb0292
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-15 23:58:34 +0530

    Time: 4 ms (25.62%), Space: 6.4 MB (71.09%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>>ans;
+       
+        vector<vector<int>> ans;
+        
         for(int i=0;i<n;i++){
-            vector<int>vec(i+1,1);
+            
+            vector<int> ds(i+1,1);
             for(int j=1;j<i;j++){
-                vec[j]=ans[i-1][j]+ans[i-1][j-1];
+                ds[j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            ans.push_back(vec);
+            ans.push_back(ds);
         }
+        
         return ans;
     }
 };
