bd7dbec5086107582e66fc3423609a5ec9490e91
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-09-27 16:56:22 +0530

    Time: 782 ms (14.95%), Space: 99.7 MB (22.42%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     
-    void dfs(int i , int j , vector<vector<int>> &vis2 , vector<vector<int>> &grid1 , vector<vector<int>> &grid2 , int &cnt1 , int &cnt2){
+    void dfs(int i , int j , vector<vector<int>> &vis2 , vector<vector<int>> &grid1 , vector<vector<int>> &grid2 , int &ans ){
         
         if(i<0 || i>=grid2.size() || j<0 || j>=grid2[0].size() || vis2[i][j]==1 || grid2[i][j]==0) return ;
-            
-        if(grid1[i][j]==1 && grid2[i][j]==1){
-            cnt1++;
-        }
-        vis2[i][j] =1;
-        cnt2++;
         
-        dfs(i-1 , j , vis2 , grid1 , grid2 , cnt1 , cnt2);
-        dfs(i , j+1 , vis2 , grid1 , grid2 , cnt1 , cnt2);
-        dfs(i+1 , j , vis2 , grid1 , grid2 , cnt1 , cnt2);
-        dfs(i , j-1 , vis2 , grid1 , grid2 , cnt1 , cnt2);
+        if(grid1[i][j]==0) ans =0;
+        vis2[i][j] =1;
+        
+        
+        dfs(i-1 , j , vis2 , grid1 , grid2 , ans);
+        dfs(i , j+1 , vis2 , grid1 , grid2 , ans);
+        dfs(i+1 , j , vis2 , grid1 , grid2 , ans);
+        dfs(i , j-1 , vis2 , grid1 , grid2 , ans);
         
     }
     
@@ -34,10 +32,10 @@ public:
         for(int i=0 ;i<n1;i++){
             for(int j=0 ;j<m1 ;j++){
                 if(grid2[i][j]==1 && !vis2[i][j]){
-                    int cnt1=0;
-                    int cnt2=0;
-                    dfs(i , j , vis2 , grid1 , grid2 , cnt1 , cnt2);
-                    if(cnt1 == cnt2) cnt++;
+                    int ans=1;
+                    
+                    dfs(i , j , vis2 , grid1 , grid2 , ans);
+                    cnt+=ans;
                     
                 }
             }
