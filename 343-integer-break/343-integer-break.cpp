d0d919885224d49662cc39bc6a0a4ca7eaf7ac7f
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-04 20:31:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (50.32%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int solve(int n , vector<int> &dp){
+        if(n==1) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        int ans = 1*(n-1);
+        for(int i=1 ; i<n ;i++){
+             int prod= i* max(n-i , solve(n-i , dp));
+             if(prod > ans) ans=prod;
+            
+        }
+        
+        return dp[n]=ans;
+    }
+    
+    int integerBreak(int n) {
+        vector<int> dp(n+1 , -1);
+        return solve(n , dp);
+    }
+};
