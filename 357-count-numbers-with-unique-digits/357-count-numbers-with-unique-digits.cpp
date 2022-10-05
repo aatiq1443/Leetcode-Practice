794a4d1d2ff9d9b022da70d7736b81024214450a
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-05 15:24:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.27%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
-//         int temp = pow(10 , n);
-//         // cout<<temp;
-//         long long int cnt=0;
-//         for(int i=10 ; i<temp ; i++){
-//             int nums = i;
-//             int size=0;
+        if(n==0) return 1; 
+        
+        if(n==10) return 10;
+        
+        int dp[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 10;
+        
+        long long int digit = 9;
+        
+        for(int i=2; i<=n ;i++){
+            digit = digit * (9 - (i - 2));
             
-//             while(nums!=0){
-//                 size++;
-//                 cout<<nums<<" ";
-//                 nums= nums/10;
-//                 cout<<nums<<" "<<size<<"      ";
-//             }
-//             // cout<<size<<" ";
-//             if(size==n && ) cnt++;
-//         }
-        
-//         return n==1?cnt+1 :cnt ;
-        
-         if(n == 0)
-            return 1;
-        
-        int ans = 10, calc = 9, temp = 9;
-        
-        for(int i = 0; i < n - 1; i++) {
-            calc *= temp;
-            ans += calc;
-            temp--;
+            dp[i] = digit;
+            
+            dp[i] = dp[i]+ dp[i-1];
         }
         
-        return ans;
-      
-        
+        return dp[n];
     }
 };
+
