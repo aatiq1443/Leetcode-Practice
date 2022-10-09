6a635c6271ceae60bf020af7944799828c114c92
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-09 13:52:57 +0530

    Time: 36 ms (47.55%), Space: 26.4 MB (44.16%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,37 +12,40 @@
 class Solution {
 public:
     
-    int findPosition(vector<int> &inorder , int element , int n){
-        for(int i=0;i<n;i++){
-            if(inorder[i]==element){
-                return i;
-            }
-        }
-        return -1;
-    }
+
     
-    TreeNode *solve(vector<int>& inorder, vector<int>& postorder , int &index , int inStart , int inEnd , int n){
+    TreeNode *solve(vector<int>& inorder, vector<int>& postorder , int &index , int n , int inSt , int inEnd , map<int,int> &mp){
         
-        if(index < 0 || inStart > inEnd){
-            return NULL;
-        }
+        if(index <0 || inSt > inEnd) return NULL;
         
         int element = postorder[index--];
-        TreeNode * root= new TreeNode(element);
-        int pos = findPosition(inorder , element , n);
         
-        root-> right = solve(inorder , postorder , index , pos+1 , inEnd , n);
-        root-> left = solve(inorder , postorder , index , inStart , pos-1 , n);
+        TreeNode *root = new TreeNode(element);
+        
+        int pos = mp[element];
+        
+        
+        root->right = solve(inorder , postorder , index , n , pos+1 , inEnd , mp);
+        root->left = solve(inorder , postorder , index , n , inSt , pos-1 , mp);
         
         return root;
+        
     }
+    
     
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         
-        int n=postorder.size();
-        int postOrIndex=n-1;
+        int n  = postorder.size();
         
-        TreeNode * ans=solve(inorder , postorder , postOrIndex , 0 , n-1 , n);
-        return ans;
+        int postInd = n-1;
+        
+        map<int , int> mp;
+        
+        for(int i=0 ; i<n ;i++){
+            mp[inorder[i]] = i;
+        }
+       
+        return solve(inorder , postorder , postInd , n , 0 , n-1 , mp);
+        
     }
 };
