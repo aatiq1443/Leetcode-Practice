c452b99d7938901e760517a4c3cf36c83fda2621
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-09 21:08:21 +0530

    Time: 80 ms (42.76%), Space: 34.7 MB (65.96%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -16,17 +16,13 @@ public:
         
        if(root==NULL) return NULL;
         
-        while(root!=NULL){
-            if(root->val==val){
-                return root;
-            }
-            else if(root->val<val){
-                root=root->right;
-            }
-            else{
-                root=root->left;
-            }
-        }
-        return NULL;
+       if(root-> val == val) return root;
+        
+       if(root->val > val) 
+           return searchBST(root->left , val);
+       else 
+           return searchBST(root->right , val);
+        
+        
     }
 };
