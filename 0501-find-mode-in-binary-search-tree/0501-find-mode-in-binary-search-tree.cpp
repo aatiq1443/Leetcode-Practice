966263ec6d393b94f30ac7e383a42fe6bef6b6b8
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-11 23:46:04 +0530

    Time: 51 ms (24.90%), Space: 25.1 MB (67.46%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,29 +11,29 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode *root, unordered_map<int,int> &mp)
+    void solve(TreeNode *root, unordered_map<int,int> &mp)
     {
         if(root == NULL)
             return;
         
         mp[root->val]++;
         
-        traverse(root->left,mp);
-        traverse(root->right,mp);
+        solve(root->left,mp);
+        solve(root->right,mp);
     }
     vector<int> findMode(TreeNode* root) 
     {
         unordered_map<int,int> mp;
-        traverse(root,mp);
+        
+        solve(root,mp);
         vector<int> ans;
         int mode = 0;
-        for(auto it:mp)
-        {
+        
+        for(auto it:mp){
             mode = max(mode,it.second);
         }
         
-        for(auto it:mp)
-        {
+        for(auto it:mp){
             if(it.second == mode)
             ans.push_back(it.first);
         }
