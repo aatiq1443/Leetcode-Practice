98aa96f9fb7555c18fd272c017f6d1142401a52e
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-10-30 14:46:53 +0530

    Time: 75 ms (75.12%), Space: 18.9 MB (34.29%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     
- int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int low = 1;
-        int high = 10000000000;
-        
-        int ans = 0;
-        while(low<=high){
-            int mid = (low+high)/2;
-            int time = timefnc(mid,piles);
-            
-            if(time<=h){
-                ans = mid;
-                high = mid-1;
-            }
-            
-            else low = mid+1;
-            
-        }
-        
-        return ans;
-        
-    }
-    
-    int timefnc(int mid,vector<int>&piles)
-    {
+    int solve(vector<int> &piles , int mid){
         int sum = 0;
-         
-        for(int i=0;i<piles.size();i++){
-            
-            sum+=(piles[i]/mid)+((piles[i]%mid)!=0);
+        
+        for(int i=0 ; i< piles.size() ;i++){
+            if(piles[i]%mid!=0){
+                sum = (sum+(piles[i]/mid))+1;
+            }
+            else{
+                sum = (sum+(piles[i]/mid));
+            }
         }
         
         return sum;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int n = piles.size();
+        
+        int st = 1;
+        int end =10000000000;
+        
+        int ans = -1;
+        while(st<=end){
+            int mid = (st+end)/2;
+            
+            int time = solve(piles , mid);
+            
+            if(time <=h){
+                ans = mid;
+                end = mid-1;
+            }
+            else st = mid+1;
+        }
+        return ans;
     }
 };
