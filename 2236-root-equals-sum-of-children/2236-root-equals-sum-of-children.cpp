47822975883555cb74271b9649268c702807184d
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-11-01 07:03:21 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (14.58%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -20,7 +20,15 @@ public:
         if(root->left) lsum = root->left ->val;
         if(root->right) rsum = root->right ->val;
         
-        return ( root->val == (lsum + rsum) && checkTree(root->left) && checkTree(root->right) );
+        // return ( root->val == (lsum + rsum) && checkTree(root->left) && checkTree(root->right) );
+        
+        if(root->val != (lsum + rsum)) return false;
+        
+        checkTree(root->left);
+        checkTree(root->right);
+        
+        return true;
+        
         
         
     }
