85ac0d632fb63209fa82838a088a459b790f935d
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-11-02 22:51:29 +0530

    Time: 8 ms (6.12%), Space: 6.5 MB (70.35%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+       
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<n;i++){
+            
+            vector<int> ds(i+1,1);
+            for(int j=1;j<i;j++){
+                ds[j] = ans[i-1][j] + ans[i-1][j-1];
+            }
+            ans.push_back(ds);
+        }
+        
+        return ans;
+    }
+};
