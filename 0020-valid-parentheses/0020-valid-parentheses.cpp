92caac9fbf59ca52cbd3096e4cb4d579fec6392c
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-11-07 23:24:40 +0530

    Time: 3 ms (53.50%), Space: 6.4 MB (10.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+           if(s[i] == '(' || s[i] =='{' || s[i] == '[') {
+               st.push(s[i]);
+           }
+            else{
+                if(st.size()==0) return false;
+                if(s[i]==')') {
+                    if(st.top()!='(') return false;
+                    else st.pop();
+                }
+                if(s[i]==']') {
+                    if(st.top()!='[') return false;
+                    else st.pop();
+                }
+                if(s[i]=='}') {
+                    if(st.top()!='{') return false;
+                    else st.pop();
+                }
+                
+                
+            }
+        }
+        if(st.size()==0) return true;
+        else return false;
+    }
+};
