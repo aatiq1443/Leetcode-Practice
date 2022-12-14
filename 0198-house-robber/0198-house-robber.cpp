c406aa0fa53d2bd3309030fb334fc6ef2fde3074
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-12-14 13:49:19 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int solve(vector<int>&nums , int n , vector<int> &dp){
+        
+       dp[0] = nums[0];
+        
+        for(int i=1 ; i<n ;i++){
+            int first = nums[i] ;
+            if(i>1) first+=dp[i-2];
+            
+            int second = 0 + dp[i-1];
+            
+            dp[i] = max(first , second);
+        }
+        
+        return dp[n-1];
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1 , 0);
+        return solve(nums , nums.size() , dp);
+    }
+};
+
