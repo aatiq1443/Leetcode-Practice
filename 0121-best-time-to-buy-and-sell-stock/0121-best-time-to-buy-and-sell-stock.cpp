66913371d20a2e52f6440e0774c473834a47063c
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2022-12-29 18:23:36 +0530

    Time: 143 ms (84.31%), Space: 93.4 MB (53.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,14 +3,13 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int maxi = INT_MAX;
-        int profit = -1;
+        int ans = -1;
         
-        for(int i=0 ;i<prices.size() ;i++){
-            if(maxi>prices[i]) maxi= prices[i];
+        for(int i=0 ; i< prices.size() ; i++){
+            if(maxi > prices[i]) maxi = prices[i];
             
-            profit= max(profit , abs(maxi - prices[i]));
+            ans = max(ans  ,  abs(prices[i] - maxi));
         }
-        
-        return profit;
+        return ans;
     }
 };
