64febf16ade6f23c64b8af0eac5679948906e8bc
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-01-22 00:09:48 +0530

    Time: 8 ms (96.09%), Space: 10.9 MB (29.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,16 +5,15 @@ public:
         int n =nums.size();
         unordered_map<int , int> mp;
         
-        for(int i=0 ; i<n ; i++){
-            mp[nums[i]] = i;
-        }
+
         
         for(int i=0 ; i< n ;i++){
             int temp = target - nums[i];
             
-            if(mp.find(temp) != mp.end() && mp[temp]!=i){
-                return {i , mp[temp]};
+            if(mp.find(temp) != mp.end()){
+                return {mp[temp] , i};
             }
+             mp[nums[i]] = i;
         }
         return {};        
     }
