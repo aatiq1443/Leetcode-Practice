d39788f1d3d893e77951ec8abfc4a01164a5c172
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-02-12 16:13:57 +0530

    Time: 5 ms (53.29%), Space: 11.1 MB (81.06%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,61 @@
 class Solution {
 public:
+    
+    int findMin(vector<int> &arr ){
+        
+        int n= arr.size();
+        
+        int st = 0 ;
+        int end = n-1;
+        
+        while(st < end){
+            int mid = st + (end - st)/2;
+            
+            if(arr[mid] > arr[n-1]){
+                st = mid+1;
+            }
+            else {
+                end = mid;
+            }
+            
+        }
+        
+        return st;
+    }
+    
+    int binarySearch(vector<int> &ans , int target , int st ,int end){
+        
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            
+            if(ans[mid] == target) return mid;
+            
+            else if(ans[mid] < target) st = mid+1;
+            
+            else  end = mid-1;
+        }
+        
+        return -1;
+        
+    }
+    
     int search(vector<int>& nums, int target) {
         
         int st = 0 ;
         int end = nums.size() -1 ;
         
-        while(st <= end){
-            
-            int mid = st + (end - st)/2;
-            
-            if(nums[mid] == target) return mid;
-            
-            if(nums[st] <= nums[mid]){
-                if(target >=nums[st] && target <=nums[mid]){
-                    end = mid-1;
-                }
-                else st = mid+1;
-            }
-            
-            else{
-                if(target >= nums[mid] && target <=nums[nums.size()-1]){
-                    st = mid+1;
-                }
-                else end = mid-1;
-            }
-            
-        }
+        int index = findMin(nums);
         
-        return -1;
+        int first = binarySearch(nums , target , st , index-1);
+        int second = binarySearch(nums , target , index , end);
+        
+        if(first == -1 && second == -1) return -1;
+        
+         if(first == -1 && second != -1) return second;
+        
+         return first;
+        
+        
+        
     }
 };
