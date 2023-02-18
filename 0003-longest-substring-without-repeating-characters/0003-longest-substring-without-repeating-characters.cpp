05c628485c6774fb291a44ba6bf1635f5b732298
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-02-18 19:37:47 +0530

    Time: 6 ms (93.17%), Space: 8.3 MB (77.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,16 +8,17 @@ public:
         int maxLen=0;
        
         
-        while( j < s.length()){
-            pos[s[j]]++;
-            
-            while(pos[s[j]] > 1){
-                pos[s[i]]--;
-                i++;
-            }
-            maxLen = max(maxLen , j-i+1);
-            j++;
-        }
+       while(j < s.size()){
+           
+           pos[s[j]]++;
+           
+           while(pos[s[j]] > 1){
+               pos[s[i]]--;
+               i++;
+           }
+           maxLen = max(maxLen , j-i+1);
+           j++;
+       }
         
         return maxLen;
     }
