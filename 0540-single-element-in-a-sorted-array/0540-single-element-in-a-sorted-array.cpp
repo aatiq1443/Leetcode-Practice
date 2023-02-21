57bfc29b7d01a14ca102de7ea63b0f7f74e11805
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-02-21 11:05:50 +0530

    Time: 26 ms (79.42%), Space: 22.3 MB (93.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int , int> mp;
+        int n = size(nums), L = 0, R = n-1, mid;
         
-        for(auto i: nums){
-            mp[i]++;
-        }
-        
-        for(auto i:mp){
-            if(i.second == 1){
-                return i.first;
-            }
+        while(L <= R) {
             
+            mid = (L + R) >> 1;
+            bool isHalfEven = (mid-L) % 2 == 0;         
+			
+            if(mid+1 < n && nums[mid] == nums[mid+1])    // case: 1
+                if(isHalfEven) L = mid+2;
+                else R = mid-1;
+				
+            else if(mid && nums[mid] == nums[mid-1])     // case: 2
+                if(isHalfEven) R = mid-2;
+                else L = mid+1;
+				
+            else return nums[mid];                       // case: 3
         }
-        return -1;
+        return -1; 
     }
 };
