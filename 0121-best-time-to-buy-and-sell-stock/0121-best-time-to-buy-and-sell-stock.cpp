eed5b03d1fabdaeadb9b4eae0655f7246f93b2f1
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-02-25 22:10:31 +0530

    Time: 137 ms (71.67%), Space: 93.2 MB (99.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,7 +6,7 @@ public:
         int ans = -1;
         
         for(int i=0 ; i< prices.size() ; i++){
-            if(maxi > prices[i]) maxi = prices[i];
+            maxi = min(maxi , prices[i]);
             
             ans = max(ans  ,  abs(prices[i] - maxi));
         }
