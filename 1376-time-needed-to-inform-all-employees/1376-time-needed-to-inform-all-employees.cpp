ac2137654dcb391ef9b8546833c5a86356841698
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2023-06-03 23:56:22 +0530

    Time: 331 ms (51.38%), Space: 114.9 MB (78.02%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        
-        vector<vector<int>> adj(n);
-        
-        for(int i=0;i<n;i++){
-            if(manager[i]!=-1)
-                adj[manager[i]].push_back(i);
-        }
-        
-        int ans = 0;
-        queue<pair<int,int>> q;
-        
-        q.push({headID , 0});
-        
-        while(!q.empty()){
-            
-            auto top = q.front();
-            int curr = top.first;
-            int cost = top.second;
-            
-            q.pop();
-            
-            ans = max(ans , cost);  
-            
-            for(int j=0;j<adj[curr].size();j++){
-                
-                q.push({adj[curr][j] , cost + informTime[curr]});
+int dfs(int id,vector<int>&vis,vector<int>ad[],vector<int>&it){
+        vis[id]=1;
+        int ans=it[id];
+        int mx=0;
+        for(auto k : ad[id]){
+            if(!vis[k]){
+                mx= max(mx,dfs(k,vis,ad,it));
             }
         }
-        return ans;
-        
+        return ans+mx;
+    }
+    int numOfMinutes(int n, int id, vector<int>& manager, vector<int>& it) {
+        vector<int>ad[n];
+        for(int i=0;i<n;i++){
+            if(manager[i]!=-1){
+                ad[manager[i]].push_back(i);
+            }
+        }
+        vector<int>vis(n,0);
+        vis[id]=1;
+        return dfs(id,vis,ad,it);
     }
 };
